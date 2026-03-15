a500378f2eb13bd5e0d589d7cedd7b8eed9d5dbc
alecvolkert <alecvolkert@gmail.com>
2026-03-14 21:41:44 -0600

    initial

---
+++ sensors.py
@@ -0,0 +1,45 @@
+
+from bme280 import BME280 #temp/pressure/humidity
+from smbus2 import SMBus  #bus
+from ltr559 import LTR559 #
+from enviroplus import gas
+from pms5003 import PMS5003 #particulate sensor
+bus = SMBus(1)
+bme280 = BME280(i2cdev = bus)
+pms5003 = PMS5003()
+ltr550 = LTR559()
+
+def get_readings():
+    return {    
+        get_bme280(),
+        get_gas(),
+        get_particle()
+    }
+
+def get_bme280():
+    return {
+        "humidity" : bme280.get_humidity(),
+        "tempurature" : bme280.get_temperature(),
+        "altitude" : bme280.get_altitude(),
+        "pressure" : bme280.get_pressure()
+    }
+
+
+def get_gas():
+    return {
+        
+    }
+
+
+def get_particle():
+    return {
+        
+    }
+
+def get_light():
+
+
+
+
+
+
---
+++ setup.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE environment;
+\c environment;
+CREATE EXTENSION postgis;
+CREATE TABLE IF NOT EXISTS enviroReadings(
+    id SERIAL PRIMARY KEY,
+    timestamp TIMESTAMPTZ DEFAULT NOW(),
+    temperature FLOAT,
+    humidity FLOAT,
+    pressure FLOAT,
+    CO_carbon_monoxide FLOAT,
+    NO2_nitrogen_dioxide FLOAT,
+    C2H5OH_ethanol FLOAT,
+    H2_hydrogen FLOAT,
+    NH3_ammonia FLOAT,
+    CH4_methane FLOAT,
+    C3H8_propane FLOAT,
+    C4H10_isobutane FLOAT,
+    dust_ppm FLOAT
+);
