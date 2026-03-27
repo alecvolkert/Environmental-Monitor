513c43300bb13dbc96f98018053ca07ae2c19ec7
alecvolkert <alecvolkert@gmail.com>
2026-03-26 21:03:01 -0600

    updated gas

--- gas.py
+++ gas.py
@@ -13,47 +13,46 @@ channels = {
 class GAS:
     def __init__(self, i2c_dev):
         self.bus = i2c_dev
-        
 
     def contact(self, channel):
-        
         config = [
             0x40 | (channel << 4),
             0x83
         ]
-	bus.write_i2c_block_data(address, 0x01 ,config)
-	while true:
-		result = bus.read_i2c_block_data(address, 0x01 ,2)
-		if result[0] & 0x80:
-			break
+
+        bus.write_i2c_block_data(address, 0x01 ,config)
+        while true:
+            result = bus.read_i2c_block_data(address, 0x01 ,2)
+            if result[0] & 0x80:
+                break
 
         data = bus.read_i2c_block_data(address, 0x00, 2)
 
         return (data[0] << 4 | data[1] >> 4)
 
-    def to_volts(self, raw):
-        voltage = raw * (4.096/2048)
-        return voltage
+        def to_volts(self, raw):
+            voltage = raw * (4.096/2048)
+            return voltage
 
-    def to_value(self, voltage):
-        if voltage == 0:
-            return float('inf')
-        sensor_read = resistance_ref * (3.3 - voltage)/voltage
-        return sensor_read
+        def to_value(self, voltage):
+            if voltage == 0:
+                return float('inf')
+            sensor_read = resistance_ref * (3.3 - voltage)/voltage
+            return sensor_read
 
-    def get_all(self):
-        readings = {}
-        for name, channel in channels.items():
-            raw = self.contact(channel)
+        def get_all(self):
+            readings = {}
+            for name, channel in channels.items():
+                raw = self.contact(channel)
+                voltage = self.to_volts(raw)
+                value = self.to_value(voltage)
+                readings[name] = value
+            return readings
+            
+        def get_desired(self, name):
+            reading = {}
+            raw = self.contact(channels[name])
             voltage = self.to_volts(raw)
             value = self.to_value(voltage)
-            readings[name] = value
-        return readings
-        
-    def get_desired(self, name):
-        reading = {}
-        raw = self.contact(channels[name])
-        voltage = self.to_volts(raw)
-        value = self.to_value(voltage)
-        reading[name] = value
-        return reading
+            reading[name] = value
+            return reading
